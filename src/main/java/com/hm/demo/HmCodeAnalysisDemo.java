e9780d0d0b05773dd68a1842a12876ea6455225c
Mohamed Hafeez <hafeez.mohamed@acquia.com>
2020-04-25 15:48:25 +0530

    Java static code analyzer demo

---
+++ src/main/java/com/hm/demo/HmCodeAnalysisDemo.java
@@ -0,0 +1,12 @@
+package com.hm.demo;
+
+public class HmCodeAnalysisDemo {
+
+	private static String abc;
+	
+	private static String a_123_test;
+	
+	public void _abctttttttttttttttttttttttttttttttttttttttttttttttttt() {
+		System.out.println("Test");
+	}
+}
---
+++ src/main/java/com/hm/demo/HmJavaCodeAnalyserDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmJavaCodeAnalyserDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmJavaCodeAnalyserDemoApplication.class, args);
+	}
+
+}
